9874092613fa6e3183916f1eb7486af9cd650f62
Ashley Kitson <ashley.kitson@zincdigital.com>
2020-10-19 06:25:18 +0100

    fixing nbsp issues in tests

--- test/Currency/FactoryTest.php
+++ test/Currency/FactoryTest.php
@@ -59,7 +59,8 @@ class FactoryTest extends \PHPUnit\Framework\TestCase
         Factory::setLocale('en_GB');
         $crcy = Factory::create('XUA',2000);
         $this->assertInstanceOf('Chippyash\Currency\Currency', $crcy);
-        $this->assertEquals('XUA 2,000', $this->stripNbsp($crcy->display()));
+        $test = $this->stripNbsp($crcy->display());
+        $this->assertTrue($test == 'XUA 2,000' || $test == 'XUA2,000');
     }
 
     public function testCreateWillReturnCurrencyRespectingExponentsForDisplay()
